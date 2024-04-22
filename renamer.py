c2ffb69ee0632dc22cb5a2ca0b464635b844c4bd
Jorge Magno <jorgejapao00@gmail.com>
2024-04-22 16:57:33 -0300

    Now it creates output path if dont existis

--- renamer.py
+++ renamer.py
@@ -6,11 +6,16 @@ import tkinter
 
 
 def rename(folder_input):
-
+    n=str(random.randint(1000, 9999))
 
     output_path = os.path.join(os.path.expanduser("~"), f"Documents\\MeFA\\Renamed Files\\{folder_input}")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+
+
     for files in folder_input:
-        shutil.move(os.path.join(folder_input, files), os.path.join(output_path, {files+1} + files))
+        shutil.move(os.path.join(folder_input, files), os.path.join(output_path, n + '_' + files))
 
 
 
@@ -23,3 +28,5 @@ def run():
     input('Como os arquivos serão renomeados? ')
     folder_input = easygui.diropenbox()
     rename(folder_input)
+
+run()
