182f5bc6277e56bad84b13e20584e85d8cdcfd04
Jorge Magno <jorgejapao00@gmail.com>
2024-04-22 16:44:04 -0300

    General code updated

--- renamer.py
+++ renamer.py
@@ -1,10 +1,16 @@
 import os
 import easygui
 import random
+import shutil
+import tkinter
 
 
 def rename(folder_input):
-    local = folder_input
+
+
+    output_path = os.path.join(os.path.expanduser("~"), f"Documents\\MeFA\\Renamed Files\\{folder_input}")
+    for files in folder_input:
+        shutil.move(os.path.join(folder_input, files), os.path.join(output_path, {files+1} + files))
 
 
 
@@ -13,5 +19,7 @@ def rename(folder_input):
 
 
 
-folder_input = easygui.diropenbox()
-rename(folder_input)
+def main():
+    input('Como os arquivos serão renomeados? ')
+    folder_input = easygui.diropenbox()
+    rename(folder_input)
