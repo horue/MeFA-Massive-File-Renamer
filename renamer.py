4ae8b9bdd796948f232d66c4c0c375bc2a2601d6
Jorge Magno <jorgejapao00@gmail.com>
2024-04-23 14:02:17 -0300

    New function added for custom name

--- renamer.py
+++ renamer.py
@@ -3,7 +3,23 @@ import easygui
 import random
 import shutil
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q','r','s','t','u','v','w','x','y','z']
+names = ['ba', 'xe', 'za', 'ux', 'pd', 'le', 'rx']
+
+
+def rename_c(source_path, name):
+    n1 = str(random.randint(0,9))
+
+    output_path = os.path.join(os.path.expanduser("~"), f"Documents\\MeFA\\Renamed Files\\{name}")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    for files in os.listdir(source_path):
+        extension = os.path.splitext(files)[1]
+        n2=str(random.randint(1000, 9999))
+        n3=random.choice(alphabet)
+        n4=random.choice(names)
+        shutil.move(os.path.join(source_path, files), os.path.join(output_path, n3 + n1 + '_' + n2 + '_' + name + '_' + n3 + extension))
 
 
 def rename(source_path, name):
@@ -17,14 +33,18 @@ def rename(source_path, name):
         extension = os.path.splitext(files)[1]
         n2=str(random.randint(1000, 9999))
         n3=random.choice(alphabet)
-        shutil.move(os.path.join(source_path, files), os.path.join(output_path, 'h' + n1 + '_' + n2 + '_' + name + '_' + n3 + extension))
+        n4=random.choice(names)
+        shutil.move(os.path.join(source_path, files), os.path.join(output_path, n3 + n1 + '_' + n2 + '_' + name + '_' + n3 + extension))
 
 
 def run():
     name=input('Como os arquivos serão renomeados? ')
     if name == '':
         name = 'h_MeFA_renamed_image'
-    source_path = easygui.diropenbox()
-    rename(source_path, name)
+        source_path = easygui.diropenbox()
+        rename(source_path, name)
+    else:
+        source_path = easygui.diropenbox()
+        rename_c(source_path, name)
 
 run()
