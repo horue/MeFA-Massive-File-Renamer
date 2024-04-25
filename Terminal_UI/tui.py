54526bc714a5b893f5040a09b25a40798f8d35e7
Jorge Magno <jorgejapao00@gmail.com>
2024-04-25 11:52:09 -0300

    Skeleton for terminal ui created

---
+++ Terminal_UI/tui.py
@@ -0,0 +1,47 @@
+import os
+import easygui
+import random   
+import shutil
+import sys
+
+def auto_rename():
+    pass
+
+def manual_rename():
+    pass
+
+def help():
+    pass
+
+def principal_menu():
+    option=input("> ")
+    if option.lower() == ("1"):
+        auto_rename()
+    elif option.lower() == ('2'):
+        manual_rename()
+    elif option.lower() ==  ('3'):
+        help()
+    elif option.lower() == ("4"):
+        sys.exit
+    while option.lower() not in ['1', '2', '3', '4']:
+        print("Please enter a valid command.")
+        option=input("> ")
+
+
+
+
+
+def principal():
+    os.system('cls')
+    print("#"*len('# MeFA - Massive File Renamer #'))
+    print('# MeFA - Massive File Renamer #')
+    print("#"*len('# MeFA - Massive File Renamer #'))
+    print('1 — Auto Rename')
+    print('2 — Manual Rename')
+    print('3 — Help')
+    print('4 — Exit')
+    print('By horue.')
+    principal_menu()
+
+
+principal()
